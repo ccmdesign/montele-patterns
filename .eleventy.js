930d40cdac53fe5a3b76b580a2c7056c35fb18d5
Douglas Cantao <dcp.pereira@gmail.com>
2024-09-23 15:36:07 -0700

    Adding custom filter to get pages, components and elements grouped

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,54 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./_src/assets/**/*.*");
   eleventyConfig.addPassthroughCopy("./_src/css/ds.css");
 
+  eleventyConfig.addFilter("groupByFolder", function(components) {
+    return components.reduce((grouped, component) => {
+      const parts = component.inputPath.split('/');
+      parts.pop(); // Remove the file name
+      
+      // Define the desired hierarchy
+      const hierarchy = {
+        'pages': 1,
+        'prototype': 2,
+        'elements': 3,
+        'components': 4
+      };
+      
+      // Sort folder parts based on the hierarchy
+      const sortedParts = parts.sort((a, b) => {
+        const aOrder = hierarchy[a] || Infinity;
+        const bOrder = hierarchy[b] || Infinity;
+        if (aOrder !== bOrder) return aOrder - bOrder;
+        return a.localeCompare(b);
+      });
+      
+      // Create the path with the desired format
+      let formattedPath = sortedParts.map((part, index) => {
+        if (part === 'elements') return '1 - elements';
+        if (part === 'components') return '2 - components';
+
+        return part;
+      });
+      
+      if(formattedPath.indexOf('_src') == 1 && !formattedPath.includes('1-elements') && !formattedPath.includes('2-components')) {
+        formattedPath = [formattedPath[0]];
+      } else if(formattedPath.indexOf('prototype') === 1) {
+        formattedPath = [formattedPath[1]];
+      } else if(formattedPath.indexOf('1-elements') === 3) {
+        formattedPath = [formattedPath[3]];
+      } else if(formattedPath.indexOf('2-components') === 3) {
+        formattedPath = [formattedPath[3]];
+      }
+
+      if (!grouped[formattedPath]) {
+        grouped[formattedPath] = [];
+      }
+      
+      grouped[formattedPath].push(component);
+      return grouped;
+    }, {});
+  });
+
   return {
     dir: {
       input: "_src",
